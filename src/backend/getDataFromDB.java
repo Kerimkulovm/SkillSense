fd9f4be97ac5f5ef8bbcf5d0d55595a8efbc086f
aumut <askarbekova.umut.kal@gmail.com>
2021-09-23 16:10:23 +0600

    Version_1.6

---
+++ src/backend/getDataFromDB.java
@@ -0,0 +1,36 @@
+package backend;
+import backend.MyConnection;
+import java.sql.*;
+
+public class getDataFromDB {
+
+
+    public static void testClick() {
+        System.out.println("Clicked !!!!");
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=MineTraining; username=MineTraining;password=qazwsx";
+
+            Connection conn = DriverManager.getConnection(url);
+            Statement statement = conn.createStatement();
+            ResultSet resultSet;
+            System.out.println("qweqweqwe");
+            resultSet = statement.executeQuery("select top 1   * from dbo.crew");
+
+            String crew = "";
+            if (!resultSet.next())
+                throw new SQLException("aaaaaaaaaaa");
+            crew = resultSet.getString("Crew");
+
+            System.out.println(crew);
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+
+        }
+    }
+
+
+
+}
