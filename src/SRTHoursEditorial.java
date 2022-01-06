f313d56d412502d1bf7ceb38a28a73ebe043e8bd
aumut <askarbekova.umut.kal@gmail.com>
2022-01-06 18:04:49 +0600

    Опечатка

--- src/SRTHoursEditorial.java
+++ src/SRTHoursEditorial.java
@@ -370,7 +370,7 @@ public class SRTHoursEditorial extends JPanel {
         private JTable listOfEmployees_table;
 
         private java.util.List<String> employeeNames_list = new ArrayList<>();
-        private List<Integer> employeeID_list = new ArrayList<>();
+        private List<String> employeeID_list = new ArrayList<>();
 
         private JPanel pageTitlePanel, tablePanel, backgroundPanel;
 
